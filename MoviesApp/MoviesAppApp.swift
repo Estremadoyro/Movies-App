ad1aa7874b86b944723e31585aaf5c95e707e78d
Leonardo <lmestremadoyro@gmail.com>
2024-09-20 21:13:09 -0500

    first commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Leonardo on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
